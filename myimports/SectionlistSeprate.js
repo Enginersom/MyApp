d4e596df177d34fefa31d0931d2b8259b848b9aa
Mr.Arabow <hanoolaato.so@gmail.com>
2022-11-02 14:30:53 +0300

    Third Commit

--- myimports/SectionlistSeprate.js
+++ myimports/SectionlistSeprate.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
         paddingRight: 10,
         paddingBottom: 2,
         backgroundColor: 'rgba(247,247,247,1.0)',
-        color: 'red',
+        color: 'blue',
     },
     title: {
         fontSize: 20,
@@ -71,8 +71,7 @@ const DATA = [
     {
         title: 'Electronics',
         data: [
-            { 'name': 'Lenovo ThinkBook', 'image': 'https://picsum.photos/id/2
-            /200/300', 'price': '$200' },
+            { 'name': 'Lenovo ThinkBook', 'image': 'https://picsum.photos/id/1/200/300', 'price': '$200' },
             { 'name': 'HP Pavilion Series', 'image': 'https://picsum.photos/id/2/200/300', 'price': '$150' },
             { 'name': 'Microsoft Surface Book 2 15', 'image': 'https://picsum.photos/id/3/200/300', 'price': '$90' },
         ],
